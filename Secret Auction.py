1feb9e651eedcb4bab818ce1608eeb3ae9ec65a3
Moketla <156257858+KayBeeeee@users.noreply.github.com>
2024-09-11 23:10:37 +0200

    Add files via upload

---
+++ Secret Auction.py
@@ -0,0 +1,14 @@
+#Secret Auction
+# This program simulates a secret auction where bidders place bids and the highest bid wins.
+# It uses a dictionary to store the bidder's name and their bid amount.
+# The program keeps track of the highest bid and the winner.
+
+
+
+
+
+
+   
+
+    
+        
