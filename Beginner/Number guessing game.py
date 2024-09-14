54a6b3c31ca673e9f6ddf2d0551fafee67890dc5
Moketla <156257858+KayBeeeee@users.noreply.github.com>
2024-09-14 03:48:31 +0200

    Final

--- Beginner/Number guessing game.py
+++ Beginner/Number guessing game.py
@@ -1,58 +1,59 @@
 import random
 
 # Number guessing Game:
-#Rules:
-
+# Rules:
 # 1. The computer will randomly generate a number between 1 and 100.
 # 2. Prompt the user to enter a number between 1 and 100.
 # 3. If the user's guess is correct, display "Congratulations! You guessed the number correctly."
 # 4. If the user's guess is too high, display "Your guess is too high. Try again."
 # 5. If the user's guess is too low, display "Your guess is too low. Try again."
 # 6. The game should continue until the user guesses the correct number.
-#The user will have a limited number of attempts to guess the number.
-# Attemps = 7
+# The user will have a limited number of attempts to guess the number.
+# Attempts = 7
 
 # Generate a random number between 1 and 100
-
 number = random.randint(1, 100) 
 attempts = 7
-
 
 print("Welcome to the Number Guessing Game!")
 print("I'm thinking of a number between 1 and 100.")
 print("You have 7 attempts to guess the number.")
 print("Let's begin!") 
 
-numberguess =int(input("Please guess a number between 1 and 100: "))
-
-def guess(numberguess):
-    global attempts
-    if numberguess < number:
-        attempts -= 1
-        print(f"Your guess is too low. Try again. You have {attempts} attempts left.")
-    elif numberguess > number:
-        attempts -= 1
-        print(f"Your guess is too high. Try again. You have {attempts} attempts left.")
-    elif numberguess == number:
-        print("Congratulations! You guessed the number correctly.")
-    else:
-        print("Invalid input. Please enter a number between 1 and 100.")
-
-    if attempts > 0 and numberguess != number:
-        numberguess = int(input("Please guess a number between 1 and 100: "))
-        guess(numberguess)
+while True:
+    try:
+        # Prompt the user to guess a number
+        numberguess = int(input("Please guess a number between 1 and 100 (or enter 0 to stop): "))
+        
+        if numberguess == 0:
+            print("You entered 0. Exiting the program.")
+            break  # Exit the loop and stop the program when 0 is entered
+        
+        # If the guess is outside the valid range, warn the user
+        if numberguess < 1 or numberguess > 100:
+            print("Invalid input. Please enter a number between 1 and 100.")
+            continue
+        
+        # If the guess is correct
+        if numberguess == number:
+            print("Congratulations! You guessed the number correctly.")
+            break
+        
+        # If the guess is too low
+        elif numberguess < number:
+            attempts -= 1
+            print(f"Your guess is too low. Try again. You have {attempts} attempts left.")
+        
+        # If the guess is too high
+        elif numberguess > number:
+            attempts -= 1
+            print(f"Your guess is too high. Try again. You have {attempts} attempts left.")
+        
+        # If the user runs out of attempts
         if attempts == 0:
-            print("Sorry, you have no more attempts left. The number was", number)
-            print("Better luck next time!")
+            print(f"Sorry, you have no more attempts left. The number was {number}. Better luck next time!")
+            break
 
-            # Run the game
-guess(numberguess)
-
-
-
-
-
-
-
-
-
+    except ValueError:
+        print("That was not a valid number. Please try again.")  # Ask for input again if input is invalid
+  
