88478c46f09abfd3ae95c4c070ae3e55153256a8
Moketla <156257858+KayBeeeee@users.noreply.github.com>
2024-09-15 23:15:32 +0200

    Add files via upload

---
+++ Beginner/Python ADT.py
@@ -0,0 +1,4 @@
+  Using the Python ADT class binary-tree, write a Python program that creates a binary tree where:
+ Each internal node stores a single character.
+ The pre-order traversal of the tree results in "EXAMFUN".
+  The in-order traversal of the tree results in "MAFXUEN".
