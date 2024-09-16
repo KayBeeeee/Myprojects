b0646c5f0aeda30bac1d5cf95ccdfce3cf2b9d6f
Moketla <156257858+KayBeeeee@users.noreply.github.com>
2024-09-17 00:55:32 +0200

    update

--- Internediate/TheCoffeeMachine.py
+++ Internediate/TheCoffeeMachine.py
@@ -27,13 +27,92 @@ MENU = {
     }
 }
 
+def coffeeMachine():
+    print("Welcome to the Coffee Machine!")
+    print("What would you like? (espresso/latte/cappuccino): ")
+    coffee = input()
+#TODO: check for ingredients at this step  
+    
+    if coffee == "espresso":
+        print("Please insert coins.")
+        quarters = int(input("How many quarters?: ")) # 0.25
+        dimes = int(input("How many dimes?: ")) # 0.10
+        nickles = int(input("How many nickles?: ")) # 0.05
+        pennies = int(input("How many pennies?: ")) # 0.01
+        total = (quarters * 0.25) + (dimes * 0.10) + (nickles * 0.05) + (pennies * 0.01)
+        if total >= MENU["espresso"]["cost"]:
+            change = total - MENU["espresso"]["cost"]
+            print(f"Here is your change: ${change}")
+            print("Here is your espresso. Enjoy!")
+        else:
+            print("Sorry, that's not enough money. Money refunded.")
+    elif coffee == "latte":
+        print("Please insert coins.")
+        quarters = int(input("How many quarters?: ")) # 0.25
+        dimes = int(input("How many dimes?: ")) # 0.10
+        nickles = int(input("How many nickles?: ")) # 0.05
+        pennies = int(input("How many pennies?: ")) # 0.01
+        total = (quarters * 0.25) + (dimes * 0.10) + (nickles * 0.05) + (pennies * 0.01)
+        if total >= MENU["latte"]["cost"]:
+            change = total - MENU["latte"]["cost"]
+            print(f"Here is your change: ${change}")
+            print("Here is your latte. Enjoy!") 
+        else:
+            print("Sorry, that's not enough money. Money refunded.")
 
-input("What would you like? (espresso/latte/cappuccino): ")
-print("Please insert coins.")
-quarters = int(input("How many quarters?: ")) # 0.25
-dimes = int(input("How many dimes?: ")) # 0.10  
-nickles = int(input("How many nickles?: ")) # 0.05
-pennies = int(input("How many pennies?: ")) # 0.01
-total = (quarters * 0.25) + (dimes * 0.10) + (nickles * 0.05) + (pennies * 0.01)
+    elif coffee == "cappuccino":
+        print("Please insert coins.")
+        quarters = int(input("How many quarters?: ")) # 0.25
+        dimes = int(input("How many dimes?: ")) # 0.10
+        nickles = int(input("How many nickles?: ")) # 0.05
+        pennies = int(input("How many pennies?: ")) # 0.01
+        total = (quarters * 0.25) + (dimes * 0.10) + (nickles * 0.05) + (pennies * 0.01)
+        if total >= MENU["cappuccino"]["cost"]:
+            change = total - MENU["cappuccino"]["cost"]
+            print(f"Here is your change: ${change}")
+            print("Here is your cappuccino. Enjoy!")
+        else:
+            print("Sorry, that's not enough money. Money refunded.")
+
+    else: 
+        print("Invalid input. Please try again.")
+        
+coffeeMachine()
+
+
+def checkIngredients(coffee):
+    if coffee == "espresso":
+        if MENU["espresso"]["ingredients"]["water"] < 50: 
+            print("Not enough water.")
+        elif MENU["espresso"]["ingredients"]["coffee"] < 18:
+            print("Not enough coffee.")
+        else:
+            print("Enough ingredients.")
+    elif coffee == "latte":
+        if MENU["latte"]["ingredients"]["water"] < 200:
+            print("Not enough water.")
+        elif MENU["latte"]["ingredients"]["milk"] < 150:
+            print("Not enough milk.")
+        elif MENU["latte"]["ingredients"]["coffee"] < 24:
+            print("Not enough coffee.")
+        else:
+            print("Enough ingredients.")
+    elif coffee == "cappuccino":
+        if MENU["cappuccino"]["ingredients"]["water"] < 250:
+            print("Not enough water.")
+        elif MENU["cappuccino"]["ingredients"]["milk"] < 100:
+            print("Not enough milk.")
+        elif MENU["cappuccino"]["ingredients"]["coffee"] < 24:
+            print("Not enough coffee.")
+        else:
+            print("Enough ingredients.")
+    else:
+        print("Invalid input.")
+
+
+
+        
+
+
 
 
