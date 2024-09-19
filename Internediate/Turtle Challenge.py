22b90f258f3163d00f87d4ab710fe61618900726
Moketla <156257858+KayBeeeee@users.noreply.github.com>
2024-09-19 13:48:34 +0200

    update

---
+++ Internediate/Turtle Challenge.py
@@ -0,0 +1,14 @@
+from turtle import Turtle
+
+# This progpram use the turtle module to draw a square
+# Draw a dashed line
+# Draw different shapes
+# Geneare random walk
+
+
+
+
+
+
+
+timmy = Turtle()
