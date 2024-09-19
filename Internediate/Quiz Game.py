894b35e0015d1c3b75d9bc48709d1d55be79d67e
Moketla <156257858+KayBeeeee@users.noreply.github.com>
2024-09-19 13:22:43 +0200

    update

--- Internediate/Quiz Game.py
+++ Internediate/Quiz Game.py
@@ -1,16 +1,7 @@
-#prettytable is a python library that allows us to create tables in the terminal
-from prettytable import PrettyTable
+#The Quiz is called True or False
 
-#creating a table object
-table = PrettyTable()
 
-#adding columns to the table
-table.field_names = ["Pokemon Name", "Type"]
 
-#adding rows to the table
-table.add_row(["Pikachu", "Electric"])
-table.add_row(["Squirtle", "Water"])
-table.add_row(["Charmander", "Fire"])
+class User():
 
-#displaying the table
-print(table)
+    
