14915b66d76871abcae1fc2ba265dc87c3c88255
Moketla <156257858+KayBeeeee@users.noreply.github.com>
2024-10-07 15:17:56 +0200

    Update

--- Internediate/Quiz Game.py
+++ Internediate/Quiz Game.py
@@ -1,6 +1,26 @@
 #The Quiz is called True or False
+#The user will be asked a list of questions
+# If True:
+#Score=+1
+# If False:
+#Score=+0
+#At the end of the quiz, the user's final score will be displayed
+#The user can choose to play again or exit the program
+#The user can also view their previous scores
+#The user can also add their own questions to the quiz
+
+from data imp
 
 
+class Question():
 
-class User():
+
+    def __init__(self, prompt, answer):
+        self.prompt = prompt
+        self.answer = answer
+
+
+    
+
+    
 
---
+++ Internediate/main.py
@@ -0,0 +1,9 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+Screen = Screen()
+
+
+
+def move_forward():
+    tim.forward()
