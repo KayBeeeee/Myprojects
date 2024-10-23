df60eb99364b70ebc17304c09930b0dfafcc3c21
Moketla <156257858+KayBeeeee@users.noreply.github.com>
2024-10-23 23:56:46 +0200

    Add files via upload

---
+++ Internediate/The ISS Notifier Project.py
@@ -0,0 +1,4 @@
+#The ISS Overhead Notifier Project
+# If the ISS is close to my current position and it is night time 
+#Then send me a notification to look 
+#We will use the ISS API for this project
