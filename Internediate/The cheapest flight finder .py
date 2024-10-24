a1fbecb3be0e73ee8fb5591c0104017982ed77eb
Moketla <156257858+KayBeeeee@users.noreply.github.com>
2024-10-24 22:26:27 +0200

    update

--- Internediate/The cheapest flight finder .py
+++
@@ -1,5 +0,0 @@
-#The cheapest flight finder App
-#This App finds the cheapest flights Using the Flight API
-# We will build our GUI using Tkinter
-#We will also send an sms using Twilio when we do find the cheapest flight
-
