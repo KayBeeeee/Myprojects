57ee2c310e4187165a0aee660c754881907d637c
Moketla <156257858+KayBeeeee@users.noreply.github.com>
2024-10-24 22:25:54 +0200

    Add files via upload

---
+++ Internediate/The playlist App.py
@@ -0,0 +1,6 @@
+#The python time machine
+# This App will give the user songs from a certain time/ year
+#We will soup to scrap the top 100 songs
+#We will use the spotify API to add the songs to our playlist
+
+
